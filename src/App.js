ce2ad8b944bf851f0595350c55f27d19bd4bbb0d
Zack McKenna <zackmckenna@gmail.com>
2020-06-17 15:24:20 -0400

    added adaptable currency adjustment

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import logo from './mintbean.png';
 import './styles/index.css';
 import axios from 'axios'
 
-import { Form, Button, Container, Col, Row } from 'react-bootstrap'
+import { ListGroup, Form, Button, Container, Col, Row } from 'react-bootstrap'
 
 function App() {
   const [input, setInput] = useState(1)
   const [data, setData] = useState(null)
   const [width, setWidth] = useState(1)
+  const [filter, setFilter] = useState('')
+  const [baseCurrency, setBaseCurrency] = useState('USD')
+  const [baseRate, setBaseRate] = useState(1)
 
-  let listItems = []
+  useEffect(() =>{
+    axios.get('https://v6.exchangerate-api.com/v6/57b7f2436e214a76bc0127f5/latest/USD')
+                          .then(res => {
+                            const data = res.data
+                            console.log(data)
+                            setData(Object.entries(data.conversion_rates).map(item => item))
+                          })
+  }, [])
 
   const handleInputChange = (event) => {
     event.preventDefault()
@@ -18,8 +28,17 @@ function App() {
   }
 
   const handleWidthChange = (event) => {
+    if (event.target.value <=1) {
+      setWidth(1)
+    } else {
+      event.preventDefault()
+      setWidth(event.target.value)
+    }
+  }
+
+  const handleFilterChange = (event) => {
     event.preventDefault()
-    setWidth(event.target.value)
+    setFilter(event.target.value)
   }
 
   const logData = (event) => {
@@ -46,7 +65,7 @@ function App() {
     return (
       <Row key={index}>
         <Col>
-          <div style={{ color: 'red',  width: rate }}>
+          <div style={{ color: 'red',  width: rate  }}>
             {name}
           </div>
         </Col>
@@ -56,21 +75,40 @@ function App() {
 
   const checkData = () => {
     if(data) {
-      Object.entries(data.conversion_rates).map((item, index) => {
+      Object.entries(data.conversion_rates).filter(item => item[0].includes(filter)).map((item, index) => {
         currency(index, item.rate, item.name)
       })
     }
   }
 
-  const renderData = (data) => {
+  const handleBaseCurrencyChange = (base, baseRate) => {
+    setBaseCurrency(base)
+    setBaseRate(baseRate)
+  }
+
+  const renderData = (data, baseRate) => {
     if(data) {
+      if(filter != ''){
+        return data.filter(item => item[0].toLowerCase().includes(filter.toLowerCase())).map(item => {
+          return (
+            <>
+              <ListGroup.Item>{item[0]}</ListGroup.Item>
+              <div style={{ backgroundColor: 'red', height: '20px', width: item[1] * input * width }}>
+                  {input / item[1] * baseRate}
+                  {baseRate}
+              </div>
+            </>
+          )
+        })
+      }
       console.log('there is data')
       return data.map(item => {
         return (
           <>
-            <li>{item[0]}</li>
+            <ListGroup.Item>{input} {baseCurrency} = {input/baseRate * item[1]} {item[0]}</ListGroup.Item>
+            <Button onClick={() => handleBaseCurrencyChange(item[0], item[1])}>Set as Base Currency</Button>
             <div style={{ backgroundColor: 'red', height: '20px', width: item[1] * input * width }}>
-                {item[1] * input}
+                {input / baseRate * item[1] }
             </div>
           </>
         )
@@ -96,23 +134,25 @@ function App() {
         </Button>
         <Form onSubmit={e => handleSubmit(e)}>
           <Form.Group controlId="formBasicEmail">
+            <Form.Label>Amount</Form.Label>
             <Form.Control onChange={event => handleInputChange(event)} type="text" placeholder="enter target rate" />
           </Form.Group>
-
           <Form.Group controlId="formBasicPassword">
-            <Form.Label>Comparison Width</Form.Label>
+            <Form.Label>Comparison Scale</Form.Label>
             <Form.Control onChange={event => handleWidthChange(event)} type="text" placeholder="comparison width" />
           </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Filter results</Form.Label>
+            <Form.Control onChange={event => handleFilterChange(event)} type="text" placeholder="filter results" />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          {/* <Button variant="primary" type="submit">
             Submit
-          </Button>
-          <h2>The </h2>
-          <ul>
-            {renderData(data)}
-          </ul>
+          </Button> */}
+          <h2>comparison scale is set at {width} pixels</h2>
+
+          <ListGroup>
+            {renderData(data, baseRate)}
+          </ListGroup>
       </Form>
       </Container>
     </div>
