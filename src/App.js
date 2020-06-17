c26cbff93ac525696316985092067ccf1f251e22
Zack McKenna <zackmckenna@gmail.com>
2020-06-17 15:50:08 -0400

    added controls in nav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './mintbean.png';
 import './styles/index.css';
 import axios from 'axios'
 
-import { ListGroup, Form, Button, Container, Col, Row } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, FormControl, ListGroup, Form, Button, Container, Col, Row } from 'react-bootstrap'
 
 function App() {
   const [input, setInput] = useState(1)
@@ -86,32 +86,48 @@ function App() {
     setBaseRate(baseRate)
   }
 
+  const currencyComponent = (item) => {
+    return (
+      <>
+      <ListGroup.Item onClick={() => handleBaseCurrencyChange(item[0], item[1])}>
+        <Row >
+          <Col>
+          {item[0]}
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div style={{ backgroundColor: 'red', height: '20px', width: item[1] * input * width }}>
+                {input / baseRate * item[1] }
+            </div>
+          </Col>
+        </Row>
+      </ListGroup.Item>
+      </>
+    )
+  }
+
   const renderData = (data, baseRate) => {
     if(data) {
       if(filter != ''){
         return data.filter(item => item[0].toLowerCase().includes(filter.toLowerCase())).map(item => {
-          return (
-            <>
-              <ListGroup.Item>{item[0]}</ListGroup.Item>
-              <div style={{ backgroundColor: 'red', height: '20px', width: item[1] * input * width }}>
-                  {input / item[1] * baseRate}
-                  {baseRate}
-              </div>
-            </>
-          )
+          // return (
+          //   <>
+          //     <ListGroup.Item>{item[0]}</ListGroup.Item>
+          //     <div style={{ backgroundColor: 'red', height: '20px', width: item[1] * input * width }}>
+          //         {input / item[1] * baseRate}
+          //         {baseRate}
+          //     </div>
+          //   </>
+          // )
+          currencyComponent(item)
         })
       }
       console.log('there is data')
       return data.map(item => {
-        return (
-          <>
-            <ListGroup.Item>{input} {baseCurrency} = {input/baseRate * item[1]} {item[0]}</ListGroup.Item>
-            <Button onClick={() => handleBaseCurrencyChange(item[0], item[1])}>Set as Base Currency</Button>
-            <div style={{ backgroundColor: 'red', height: '20px', width: item[1] * input * width }}>
-                {input / baseRate * item[1] }
-            </div>
-          </>
-        )
+        return currencyComponent(item)
       })
     } else {
       return <h2>no data</h2>
@@ -119,43 +135,61 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Col>
-            <h1>CURRENT</h1>
-          </Col>
-        </Row>
-        <Button onClick={event => logData(event)}>
-          Log data
-        </Button>
-        <Button onClick={event => checkData(event)}>
-            Check data
-        </Button>
-        <Form onSubmit={e => handleSubmit(e)}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Amount</Form.Label>
-            <Form.Control onChange={event => handleInputChange(event)} type="text" placeholder="enter target rate" />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Comparison Scale</Form.Label>
-            <Form.Control onChange={event => handleWidthChange(event)} type="text" placeholder="comparison width" />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Filter results</Form.Label>
-            <Form.Control onChange={event => handleFilterChange(event)} type="text" placeholder="filter results" />
-          </Form.Group>
-          {/* <Button variant="primary" type="submit">
-            Submit
-          </Button> */}
-          <h2>comparison scale is set at {width} pixels</h2>
+    <>
+      <Navbar sticky='top' bg="light" expand="lg">
+        <Navbar.Brand href="#home">{input} {baseCurrency} </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="Conversion Settings" id="basic-nav-dropdown">
+            <Form onSubmit={e => handleSubmit(e)}>
+              <Form.Group style={{ padding: '0 20px' }} controlId="formBasicEmail">
+                <Form.Control onChange={event => handleInputChange(event)} type="text" placeholder="enter target rate" />
+              </Form.Group>
+              <Form.Group style={{ padding: '0 20px' }} controlId="formBasicPassword">
+                <Form.Control onChange={event => handleWidthChange(event)} type="text" placeholder="comparison unit width" />
+              </Form.Group>
+              <Form.Group style={{ padding: '0 20px' }} controlId="formBasicPassword">
+                <Form.Control onChange={event => handleFilterChange(event)} type="text" placeholder="filter results" />
+              </Form.Group>
+            </Form>
+            </NavDropdown>
+            <Nav.Link target="_blank" href="https://zackmckenna.com">made by zackmckenna</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="App">
+        <Container>
+          <Row>
+            <Col>
+              <h1>current</h1>
+            </Col>
+          </Row>
+          <Form onSubmit={e => handleSubmit(e)}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Amount</Form.Label>
+              <Form.Control onChange={event => handleInputChange(event)} type="text" placeholder="enter target rate" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Comparison Scale (in pixels) </Form.Label>
+              <Form.Control onChange={event => handleWidthChange(event)} type="text" placeholder="comparison unit width" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Filter results</Form.Label>
+              <Form.Control onChange={event => handleFilterChange(event)} type="text" placeholder="filter results" />
+            </Form.Group>
+          </Form>
+            {/* <Button variant="primary" type="submit">
+              Submit
+            </Button> */}
+            <h3>comparison scale is set at {width} pixels</h3>
 
-          <ListGroup>
-            {renderData(data, baseRate)}
-          </ListGroup>
-      </Form>
-      </Container>
-    </div>
+            <ListGroup>
+              {renderData(data, baseRate)}
+            </ListGroup>
+        </Container>
+      </div>
+    </>
   );
 }
 
